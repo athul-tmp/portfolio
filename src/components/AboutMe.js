c0b24d1ae28edc02e7ec9b2009503a302d338911
Athul <s3958556@student.rmit.edu.au>
2025-06-19 23:15:05 +1000

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,8 +8,8 @@ function AboutMe() {
         <h1>Who am I?</h1>
         {/* Mobile image */}
         <img src={myImage} alt="This is me" className="about-me-img mobile-only" />
-        <p>My name is <b>Athul</b>. I'm a recent Computer Science graduate from RMIT University, passionate about computers and technology. Beyond academics, I enjoy exploring new tech, gaming, NBA, F1, and all things nerdy.</p>
-        <p>Check out some projects!</p>
+        <p>My name is <b>Athul</b>. I'm a recent Computer Science graduate from RMIT University, passionate about computers and technology. Beyond coding, I enjoy exploring new tech, gaming, NBA, F1, and all things nerdy.</p>
+        <p>Check out some of my projects!</p>
       </div>
       {/* Desktop image */}
       <img src={myImage} alt="This is me" className="about-me-img desktop-only" />
