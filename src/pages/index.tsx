2a38f2ae4c192a2f6ef5e1d254dc0aa7f8130e95
Athul <s3958556@student.rmit.edu.au>
2025-10-31 23:21:48 +1100

    improved meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,21 @@ import Head from "next/head";
 export default function HomePage() {
   return (
     <>
-    <Head>
+    {/* <Head>
       <meta property="og:title" content="Athul Thampan Portfolio" />
-        <meta property="og:description" content="Portfolio showcasing Athul Thampan's software projects." />
-        <meta property="og:url" content="https://joblog.athulthampan.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://athulthampan.com/images/portfolio.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+      <meta property="og:description" content="Portfolio showcasing Athul Thampan's software projects." />
+      <meta property="og:url" content="https://athulthampan.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://athulthampan.com/images/portfolio.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
-        <meta property="og:site_name" content="Athul Thampan" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Athul Thampan Portfolio" />
-        <meta name="twitter:description" content="Portfolio showcasing Athul Thampan's software projects." />
-        <meta name="twitter:image" content="https://athulthampan.com/images/portfolio.png" />
-    </Head>
+      <meta property="og:site_name" content="Athul Thampan" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Athul Thampan Portfolio" />
+      <meta name="twitter:description" content="Portfolio showcasing Athul Thampan's software projects." />
+      <meta name="twitter:image" content="https://athulthampan.com/images/portfolio.png" />
+    </Head> */}
     
     <div className="space-y-24">
       <AboutMeSection />     
